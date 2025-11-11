6e974233fdff924cb3854ce20338e22a98189820
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2025-11-11 17:12:25 +1100

    Removed Old markdown files

--- Software/Testing_N_Experimentation/Alpha_Release_Stack/set_dual_display_env.py
+++
@@ -1,9 +0,0 @@
-"""
-PlatformIO pre-build script to set environment variable for dual display test
-"""
-Import("env")
-import os
-
-# Set environment variable to select dual display test source
-os.environ['DUAL_DISPLAY_TEST'] = '1'
-print("Environment variable DUAL_DISPLAY_TEST set for CMake")
