0256df1385dfa972d4cb2e1bc03c903b755f4a3d
XCR-1793 <138337647+XCR1793@users.noreply.github.com>
2026-01-21 01:01:03 +1100

    Removed Obsolete systems

--- Software/Testing_N_Experimentation/Alpha_Release_Stack/include/AnimationDriver/Render/Render.hpp
+++
@@ -1,9 +0,0 @@
-/*****************************************************************
- * @file Render.hpp
- * @brief Master include for render components
- *****************************************************************/
-
-#pragma once
-
-#include "RenderTarget.hpp"
-#include "AnimationScene.hpp"
